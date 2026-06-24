6a59d3e7a93aae4b885805d7f33684c18e684cdf
san-eee <sanjaybalan.c2025eee@sece.ac.in>
2026-06-24 19:24:38 +0530

    Update form.py

--- form.py
+++ form.py
@@ -54,6 +54,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "My website is live on Render!"
+import os
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
