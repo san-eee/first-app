f7df637628710170c091d95e595faa25ab841594
Sanjay Balan C <sanjaybalan.c2025eee@sece.ac.in>
2026-06-24 18:37:16 +0530

    committed

---
+++ form.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+# create database + table (runs once safely)
+def init_db():
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        roll TEXT,
+        dept TEXT,
+        year TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.json
+
+    name = data['name']
+    roll = data['roll']
+    dept = data['dept']
+    year = data['year']
+
+    conn = sqlite3.connect("students.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO students (name, roll, dept, year)
+        VALUES (?, ?, ?, ?)
+    """, (name, roll, dept, year))
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"status": "saved"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "My website is live on Render!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
