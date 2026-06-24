7c5e6361e940580933e9ad8de4f29e134afeae87
san-eee <sanjaybalan.c2025eee@sece.ac.in>
2026-06-24 19:27:57 +0530

    Remove unnecessary blank line in form.py

--- form.py
+++ form.py
@@ -55,7 +55,6 @@ app = Flask(__name__)
 def home():
     return "My website is live on Render!"
 import os
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
